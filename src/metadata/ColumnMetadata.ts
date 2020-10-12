7e9adeaffcf462ed09accd58cc0dfdfabe41e576
Josh <MechJosh0@users.noreply.github.com>
2020-10-12 13:03:48 +0100

    Allow create mutations to handle Buffer type columns

--- src/metadata/ColumnMetadata.ts
+++ src/metadata/ColumnMetadata.ts
@@ -615,7 +615,7 @@ export class ColumnMetadata {
                 if (relatedEntity && relatedEntity instanceof Object && !(relatedEntity instanceof FindOperator) && !(relatedEntity instanceof Function)) {
                     value = this.referencedColumn.getEntityValue(relatedEntity);
 
-                } else if (entity[this.propertyName] && entity[this.propertyName] instanceof Object && !(entity[this.propertyName] instanceof FindOperator) && !(entity[this.propertyName] instanceof Function)) {
+                } else if (entity[this.propertyName] && entity[this.propertyName] instanceof Object && !(entity[this.propertyName] instanceof FindOperator) && !(entity[this.propertyName] instanceof Function) && !(entity[this.propertyName] instanceof Buffer)) {
                     value = this.referencedColumn.getEntityValue(entity[this.propertyName]);
 
                 } else {
